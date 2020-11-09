376e62b8a741fd2f660640d4fdc8c70667a3f404
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-09 15:38:49 -0800

    Write and pass test to generate list of numbers recursively

--- __tests__/prime.test.js
+++ __tests__/prime.test.js
@@ -1,7 +1,13 @@
-import { getPrimes } from './../src/js/prime.js'
+import { getPrimes, getList } from './../src/js/prime.js'
 
 describe('getPrimes', () => {
 test('returns an array of all prime numbers up to given number', () => {
-    expect(getPrimes(14)).toEqual([2, 3, 5, 7, 11, 13])
+    expect(getPrimes(14)).toEqual([2, 3, 5, 7, 11, 13]);
+  })
+})
+
+describe('getList', () => {
+test('returns an array of all numbers from 2 up to number', () => {
+    expect(getList(14)).toEqual([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
   })
 })
--- src/js/prime.js
+++ src/js/prime.js
@@ -1,15 +1,29 @@
+// Looping Solution
 export const getPrimes = (num) => {
   let arr = [];
   for (let i=2; i<num; i++){ 
     arr.push(i); 
   };
-
   let prime = 2;
-
   while (prime < num) {
     arr = arr.filter(e => e % prime !== 0 || e === prime );
     prime ++;
   }
-
   return arr;
+}
+
+// Recursive Solution
+export const getList = (num) => {
+  let list = [];
+  const generateList = (number) => {
+    list.unshift(number)
+    if (number === 2){
+      console.log("List generated!")
+      return;
+    } else {
+      return generateList(number-1);
+    }
+  }
+  generateList(num-1);
+  return list;
 }
